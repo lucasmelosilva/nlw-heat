5e3965941d52e0460788b969a0ed413f9768d629
lucas melo <lucas.melo044@gmail.com>
2021-10-19 06:01:26 -0300

    recupera o token

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
 import 'dotenv/config';
 import express from 'express';
+import { router } from './routes';
 
-const app = express()
+const app = express();
+app.use(express.json());
+
+app.use(router);
 
 app.get('/github', (req, res) => {
   res.redirect(
     `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
   )
-})
+});
 
 app.get('/signin/callback', (req,res) => {
-  const { code } = req.query
-
+  const { code } = req.query;
   return res.json(code);
-})
+});
 
 app.listen(4000, () => console.log('server is running on PORT 4000'));
 
