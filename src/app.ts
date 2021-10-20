955c70ba634a2c04dfe6d220dcdb5ec1b559ac25
lucas melo <lucas.melo044@gmail.com>
2021-10-20 10:20:37 -0300

    add socket && config conexao

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
 import 'dotenv/config';
 import express from 'express';
+import http  from 'http';
+import cors  from 'cors';
+import { Server } from 'socket.io';
+
 import { router } from './routes';
 
 const app = express();
+app.use(cors())
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", socket => {
+  console.log(`User connect on socket user_id=(${socket.id})`)
+})
+
 app.use(express.json());
 
 app.use(router);
@@ -18,5 +35,4 @@ app.get('/signin/callback', (req,res) => {
   return res.json(code);
 });
 
-app.listen(4000, () => console.log('server is running on PORT 4000'));
-
+export { serverHttp, io }
