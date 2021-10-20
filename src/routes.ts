cd88b4df614c1f485230fd7296945a8805e7e370
lucas melo <lucas.melo044@gmail.com>
2021-10-20 12:31:20 -0300

    add rota para pegar os dados do user

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessagecontroller } from "./controllers/CreateMessagecontroller";
 import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -14,6 +15,8 @@ router.post(
   new CreateMessagecontroller().handle
 );
 
-router.get('/messages/last3', new GetLast3MessagesController().handle)
+router.get("/messages/last3", new GetLast3MessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
